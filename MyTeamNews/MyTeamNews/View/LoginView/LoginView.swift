4bd8e0aacdbf5988a22d876a3f75cb48f7a6060d
baek <jihunmm02@gmail.com>
2024-09-10 16:49:05 +0900

    - Commit LoginStackView 및 메인 Label 추가

--- MyTeamNews/MyTeamNews/View/LoginView/LoginView.swift
+++ MyTeamNews/MyTeamNews/View/LoginView/LoginView.swift
@@ -10,6 +10,27 @@ import Foundation
 
 final class LoginView: UIView {
     
+    // - MARK: Properties
+    
+    private let LoginStackView: UIStackView  = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.spacing = CGFloat(LoginViewSetEnum.LoginViewStackViewSpacing)
+        return stackView
+    }()
+    
+    private let myTeamLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.textColor = .white
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.text = LoginViewSetEnum.LoginLogoText
+        return label
+    }()
     
     // - MARK: Login View Setting
     
