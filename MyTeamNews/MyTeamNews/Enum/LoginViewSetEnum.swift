eb84db41cd5dc789c91f408dfcde274fd7face08
baek <jihunmm02@gmail.com>
2024-09-10 16:44:44 +0900

    - Commit Enum 그룹  및 LoginView Enum 추가

---
+++ MyTeamNews/MyTeamNews/Enum/LoginViewSetEnum.swift
@@ -0,0 +1,14 @@
+//
+//  ViewSetEnum.swift
+//  MyTeamNews
+//
+//  Created by Baek on 9/10/24.
+//
+
+import Foundation
+
+enum LoginViewSetEnum {
+    static let LoginViewStackViewSpacing = 10
+    static let LoginButtonSpacing = 10
+    static let LoginLogoText = "MyTeam"
+}
