de55f7c62ddb4b5d2e4d5785b38b829766b8f261
baek <jihunmm02@gmail.com>
2024-09-10 16:48:16 +0900

    - Commit LoginView 파일 추가

---
+++ MyTeamNews/MyTeamNews/View/LoginView/LoginView.swift
@@ -0,0 +1,27 @@
+//
+//  LoginView.swift
+//  MyTeamNews
+//
+//  Created by Baek on 9/10/24.
+//
+import UIKit
+
+import Foundation
+
+final class LoginView: UIView {
+    
+    
+    // - MARK: Login View Setting
+    
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+extension LoginView {
+    
+}
