5283aae084493171ac2897d08d37279503242539
baek <jihunmm02@gmail.com>
2024-09-10 16:47:00 +0900

    - Commit ViewController -> LoginViewController 로 이름 변경

---
+++ MyTeamNews/MyTeamNews/View/ViewController/LoginViewController/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyTeamNews
+//
+//  Created by Baek on 9/4/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
