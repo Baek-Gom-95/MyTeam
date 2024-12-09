17ee2ae182ee100c02bc69f3b93e28eaa5bce4a2
baek <jihunmm02@gmail.com>
2024-12-09 16:27:54 +0900

    - Commit 로그인뷰 수정

--- MyTeamNews/MyTeamNews/View/LoginView/LoginView.swift
+++ MyTeamNews/MyTeamNews/View/LoginView/LoginView.swift
@@ -5,20 +5,19 @@
 //  Created by Baek on 9/10/24.
 //
 import UIKit
-
 import Foundation
 
 final class LoginView: UIView {
     
     // - MARK: Properties
     
-    private let LoginStackView: UIStackView  = {
+    private let loginStackView: UIStackView  = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.distribution = .fillEqually
         stackView.alignment = .center
         stackView.axis = .vertical
-        stackView.spacing = CGFloat(LoginViewSetEnum.LoginViewStackViewSpacing)
+        stackView.spacing = CGFloat(LoginViewSetEnum.loginViewStackViewSpacing)
         return stackView
     }()
     
@@ -28,26 +27,53 @@ final class LoginView: UIView {
         label.adjustsFontForContentSizeCategory = true
         label.textColor = .white
         label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
-        label.text = LoginViewSetEnum.LoginLogoText
+        label.text = LoginViewSetEnum.loginLogoText
         return label
     }()
     
     // - MARK: Login Button Properties
     
-    private let LoginButtonStackView: UIStackView = {
+    private let loginButtonStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.distribution = .fillEqually
+        stackView.distribution = .equalCentering
         stackView.alignment = .center
         stackView.axis = .vertical
-        stackView.spacing = CGFloat(LoginViewSetEnum.LoginButtonSpacing)
+        stackView.spacing = CGFloat(LoginViewSetEnum.loginButtonSpacing)
         return stackView
+    }()
+    
+    var kakaoLoginButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "kakao_login_normal"), for: .normal)
+        
+        return button
+    }()
+    
+    var googleLoginButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "google_login"), for: .normal)
+        
+        return button
+    }()
+    
+    var appleLoginButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "apple_login"), for: .normal)
+        
+        return button
     }()
     
     // - MARK: Login View Setting
     
     override init(frame: CGRect) {
         super .init(frame: frame)
+        self.backgroundColor = .black
+        setupUI()
+        setupConstraint()
     }
     
     required init?(coder: NSCoder) {
@@ -57,4 +83,42 @@ final class LoginView: UIView {
 
 extension LoginView {
     
+    private func setupUI() {
+        self.addSubview(loginStackView)
+        
+        loginStackView.addArrangedSubview(myTeamLabel)
+        loginStackView.addArrangedSubview(loginButtonStackView)
+        
+        loginButtonStackView.addArrangedSubview(kakaoLoginButton)
+        loginButtonStackView.addArrangedSubview(googleLoginButton)
+        loginButtonStackView.addArrangedSubview(appleLoginButton)
+    }
+    
+    private func setupConstraint(){
+        
+        // 버튼 이미지 크기 설정
+        setButtonImageSize(kakaoLoginButton, width: 280, height: 70, cornerRadius: 15)
+        setButtonImageSize(googleLoginButton, width: 280, height: 70, cornerRadius: 15)
+        setButtonImageSize(appleLoginButton, width: 280, height: 70, cornerRadius: 15)
+        
+        NSLayoutConstraint.activate([
+            loginStackView.topAnchor.constraint(equalTo: self.topAnchor),
+            loginStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            loginStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            loginStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            
+            loginButtonStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            loginButtonStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            
+                
+        ])
+    }
+    
+    private func setButtonImageSize(_ button: UIButton, width: CGFloat, height: CGFloat, cornerRadius: CGFloat) {
+        button.widthAnchor.constraint(equalToConstant: width).isActive = true
+        button.heightAnchor.constraint(equalToConstant: height).isActive = true
+        button.layer.cornerRadius = cornerRadius
+        button.layer.masksToBounds = true
+        button.imageView?.contentMode = .scaleAspectFill
+    }
 }
