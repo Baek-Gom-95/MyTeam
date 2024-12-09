e079a39c993ed25be1b122910c7619e13b730579
baek <jihunmm02@gmail.com>
2024-12-09 16:30:07 +0900

    - Commit AppDelegate 수정

--- MyTeamNews/MyTeamNews/AppDelegate.swift
+++ MyTeamNews/MyTeamNews/AppDelegate.swift
@@ -8,6 +8,8 @@
 import UIKit
 import KakaoSDKCommon
 import FirebaseCore
+import FirebaseFirestore
+import KakaoSDKAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        KakaoSDK.initSDK(appKey: "449d266bb9e2a0ad4d05dfe94f1ed875")
         FirebaseApp.configure()
+        KakaoSDK.initSDK(appKey: "449d266bb9e2a0ad4d05dfe94f1ed875")
         return true
     }
 
@@ -33,6 +35,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        return false
     }
 
 
